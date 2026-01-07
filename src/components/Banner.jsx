1e77472a4dee212d8159a065a7d2389dfca53038
23081010252 <23081010252@student.upnjatim.ac.id>
2026-01-07 17:06:11 +0700

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,16 +1,32 @@
+/**
+ * komponen banner untuk bagian banner persuasif untuk mendaftar member pada footer halaman.
+ * menampilkan gambar di sisi kiri dan teks informasi (judul & deskripsi) di sisi kanan dengan latar belakang gelap.
+ * * @component
+ * @param {object} props - properti komponen.
+ * @param {function} props.setActivePage - fungsi untuk mengatur navigasi halaman aktif.
+ * * @description
+ * komponen ini mengandalkan `react-i18next` untuk internasionalisasi (i18n).
+ * mengambil data teks dari namespace "banner" dengan kunci `banner_judul` dan `banner_deskripsi`.
+ * * @example
+ * <Banner setActivePage={(page) => console.log(page)} />
+ * * @returns {jsx.element} struktur div pembungkus dengan sistem grid, gambar, dan blok teks informasi.
+ */
+
 import React from "react";
-import { useTranslation } from "react-i18next"; // import useTranslation dari i18next untuk menyediakan pustaka bahasa
+import { useTranslation } from "react-i18next";
 
 export default function Banner({ setActivePage }) {
-  const { t } = useTranslation("banner"); // menerima pustaka dari namespace "banner" di i18next melalui fungsi t (translate)
+  const { t } = useTranslation("banner");
 
   return (
     <div className="relative gap-0 max-w-6xl mx-auto px-4 mb-5">
       <div className="grid grid-cols-[1fr_2fr] justify-between gap-0">
+        {/* gambar ilustrasi */}
         <img src="image 4.png" className="h-full hoverflow-hidden" alt="" />
+        {/* menggunakan key dari namespace banner */}
         <div className="bg-gray-900 space-y-4 p-8">
           <h2 className="text-4xl max-w-xl font-semibold text-white">
-            {t("banner_judul")} {/* menggunakan key dari namespace banner */}
+            {t("banner_judul")}
           </h2>
           <p className="text-white opacity-50 max-w-lg">
             {t("banner_deskripsi")}{" "}
